53d91d820c850a2c350e037447dfbcd01e7e6f9e
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-02-17 13:58:41 +0400

    inter

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -1,4 +1,26 @@
 package ru.easycode.zerotoheroandroidtdd
 
 interface Count {
+    fun increment(number: String): UiState
+    fun initial(number: String): UiState
+    fun decrement(number: String): UiState
+
+    class Base(step: Int, max: Int, min: Int) : Count {
+        private var count:Int = 0
+        init{
+
+        }
+        override fun increment(number: String): UiState {
+            TODO("Not yet implemented")
+        }
+
+        override fun initial(number: String): UiState {
+            TODO("Not yet implemented")
+        }
+
+        override fun decrement(number: String): UiState {
+            TODO("Not yet implemented")
+        }
+
+    }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,15 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface UiState {
+    class Min(text: String) : UiState {
+
+    }
+
+    class Base(text: String) : UiState {
+
+    }
+
+    class Max(text: String) : UiState {
+
+    }
+}
